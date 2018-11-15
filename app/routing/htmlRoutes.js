7aa3969c82e0d2d7f9706300a6df85fc45ce73b0
Bradley <brad.driver33@gmail.com>
2018-11-15 16:13:38 -0500

    coded the htmlRoutes.js file

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,16 @@
+// path is a dependency
+const path = requier('path');
+
+// routing
+module.exports = app => {
+
+    // This code creates the html GET Route for /survey to display the survey page
+    app.get("/survey", (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/survey.html'));
+    });
+
+    // This code creates the html GET Route for default to display the home page
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/home.html'));
+    });
+}
