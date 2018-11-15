37bc4545b65c212508d9e47fd5fe1efac390d7ea
Bradley <brad.driver33@gmail.com>
2018-11-15 15:57:01 -0500

    finished server.js code.I have to a couple more things to this file but I have almost complete.

---
+++ server.js
@@ -0,0 +1,19 @@
+// The dependencies for express
+const express = require('express');
+
+// configuration for express server
+const app = express();
+
+const PORT = process.env.port || 8080;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// router configuration
+// require("./app/routes/apiRoutes")(app);
+// require("./app/routes/htmlRoutes")(app);
+
+// listener - starts server
+app.listen(PORT, () => {
+    console.log(`App listening on: http://localhost:${PORT}`);
+});
