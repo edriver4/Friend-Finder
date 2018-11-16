6a923bc27af10101692394d5ff2cecbe9149a52f
Bradley <brad.driver33@gmail.com>
2018-11-16 10:54:48 -0500

    finished the friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+const friendsArr = [
+    {
+        name: "Jessica Alba",
+        photo: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE4MDAzNDEwMzY0NDMzOTM0/jessica-alba-299896-1-402.jpg",
+        scores: [1,2,4,3,5,1,4,4,5,5]
+    },
+    {
+        name: "Brad Pitt",
+        photo: "http://famepace.com/wp-content/uploads/2017/02/Young-Brad-Pitt-1.jpg",
+        scores: [1,2,3,4,5,5,5,3,4,2]
+    },
+    {
+        name: "Jessica Simpson",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Jessica_Simpson_Joining_Forces_with_the_Rockies_April_2011_%28cropped%29.jpg/220px-Jessica_Simpson_Joining_Forces_with_the_Rockies_April_2011_%28cropped%29.jpg",
+        scores: [1,2,4,5,3,4,4,5,1,3]
+    },
+    {
+        name: "Chris Hemsworth",
+        photo: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTU0ODUwMjQ0NjIwNzI0MDAx/chris-hemsworth-poses-during-a-photo-call-for-thor-ragnarok-on-october-15-2017-in-sydney-australia-photo-by-mark-metcalfe_getty-images-for-disney-square.jpg",
+        scores: [1,2,2,2,4,5,5,5,3,4]
+    },
+    {
+        name: "Russell Wilson",
+        photo: "https://moneyinc.com/wp-content/uploads/2016/06/Russell-Wilson-1-750x422.jpg",
+        scores: [1,2,4,4,4,5,5,5,3,3]
+    },
+    {
+        name: "Todd Gurley",
+        photo: "http://www.wruf.com/wp-content/uploads/2016/09/USATSI_9511513-e1473705832116-660x330.jpg",
+        scores: [1,2,4,4,5,5,5,3,2,3]
+    }
+]
+
+module.exports = friendsArr;
