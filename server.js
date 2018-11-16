59da8d53ba583c91e9ca6735b43920fe01bc7fbf
Bradley <brad.driver33@gmail.com>
2018-11-16 10:55:45 -0500

    fixed server.js code

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express();
 
 const PORT = process.env.port || 8080;
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // router configuration
-// require("./app/routes/apiRoutes")(app);
-// require("./app/routes/htmlRoutes")(app);
+require("./app/routes/apiRoutes")(app);
+require("./app/routes/htmlRoutes")(app);
 
 // listener - starts server
 app.listen(PORT, () => {
